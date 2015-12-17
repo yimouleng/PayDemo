fd31c367d539202a81cb9c578addb1c7f30d7f2e
yimouleng <wmt443040748@163.com>
2015-12-17 12:28:02 +0800

    添加二次验证
    添加二次验证

--- Pay/ViewController.m
+++ Pay/ViewController.m
@@ -109,8 +109,57 @@
     [SVProgressHUD dismiss];
     NSLog(@"------------反馈信息结束-----------------");
 }
-
-
+//沙盒测试环境验证
+#define SANDBOX @"https://sandbox.itunes.apple.com/verifyReceipt"
+//正式环境验证
+#define AppStore @"https://buy.itunes.apple.com/verifyReceipt"
+/**
+ *  验证购买，避免越狱软件模拟苹果请求达到非法购买问题
+ *
+ */
+-(void)verifyPurchaseWithPaymentTransaction{
+    //从沙盒中获取交易凭证并且拼接成请求体数据
+    NSURL *receiptUrl=[[NSBundle mainBundle] appStoreReceiptURL];
+    NSData *receiptData=[NSData dataWithContentsOfURL:receiptUrl];
+    
+    NSString *receiptString=[receiptData base64EncodedStringWithOptions:NSDataBase64EncodingEndLineWithLineFeed];//转化为base64字符串
+    
+    NSString *bodyString = [NSString stringWithFormat:@"{\"receipt-data\" : \"%@\"}", receiptString];//拼接请求数据
+    NSData *bodyData = [bodyString dataUsingEncoding:NSUTF8StringEncoding];
+    
+    
+    //创建请求到苹果官方进行购买验证
+    NSURL *url=[NSURL URLWithString:SANDBOX];
+    NSMutableURLRequest *requestM=[NSMutableURLRequest requestWithURL:url];
+    requestM.HTTPBody=bodyData;
+    requestM.HTTPMethod=@"POST";
+    //创建连接并发送同步请求
+    NSError *error=nil;
+    NSData *responseData=[NSURLConnection sendSynchronousRequest:requestM returningResponse:nil error:&error];
+    if (error) {
+        NSLog(@"验证购买过程中发生错误，错误信息：%@",error.localizedDescription);
+        return;
+    }
+    NSDictionary *dic=[NSJSONSerialization JSONObjectWithData:responseData options:NSJSONReadingAllowFragments error:nil];
+    NSLog(@"%@",dic);
+    if([dic[@"status"] intValue]==0){
+        NSLog(@"购买成功！");
+        NSDictionary *dicReceipt= dic[@"receipt"];
+        NSDictionary *dicInApp=[dicReceipt[@"in_app"] firstObject];
+        NSString *productIdentifier= dicInApp[@"product_id"];//读取产品标识
+        //如果是消耗品则记录购买数量，非消耗品则记录是否购买过
+        NSUserDefaults *defaults=[NSUserDefaults standardUserDefaults];
+        if ([productIdentifier isEqualToString:@"123"]) {
+            int purchasedCount=[defaults integerForKey:productIdentifier];//已购买数量
+            [[NSUserDefaults standardUserDefaults] setInteger:(purchasedCount+1) forKey:productIdentifier];
+        }else{
+            [defaults setBool:YES forKey:productIdentifier];
+        }
+        //在此处对购买记录进行存储，可以存储到开发商的服务器端
+    }else{
+        NSLog(@"购买失败，未通过验证！");
+    }
+}
 //监听购买结果
 - (void)paymentQueue:(SKPaymentQueue *)queue updatedTransactions:(NSArray *)transaction{
     
@@ -122,7 +171,9 @@
         switch (tran.transactionState) {
             case SKPaymentTransactionStatePurchased:{
                 NSLog(@"交易完成");
+                [self verifyPurchaseWithPaymentTransaction];
                 [[SKPaymentQueue defaultQueue] finishTransaction:tran];
+                
             }
                
                 
